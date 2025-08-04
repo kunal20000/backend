0239cf7252108965235eed602818337c8a78384d
Kunal Sapate <kunal.sapate@aticanlabs.com>
2025-08-04 09:46:19 +0530

    added api: signin,signup

--- index.js
+++ index.js
@@ -11,13 +11,22 @@ app.use(cors());
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password
   res.send(result);
 });
 
-app.post("login", async () => {
-  let user = new User(req.body);
-  let result = await user.save();
-  res.send(result);
+app.post("/login", async (req, res) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send("result: No user found");
+    }
+  } else {
+    res.send({ result: "No user found" });
+  }
 });
 
 app.listen(5000);
